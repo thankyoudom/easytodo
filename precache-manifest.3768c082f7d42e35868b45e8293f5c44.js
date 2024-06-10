f20790f5e7b820c66e1b84cc608781891265ed80
monsieurverde <domverdemgmt@gmail.com>
2024-06-10 17:12:55 -0400

    first draft

---
+++ precache-manifest.3768c082f7d42e35868b45e8293f5c44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381a4d641549af42b22cc87e49a007e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebe7e4f33ee6812983d7",
+    "url": "/static/css/2.4e15b5ff.chunk.css"
+  },
+  {
+    "revision": "ec780b8b31151f3cb3bb",
+    "url": "/static/css/main.0e80c2a5.chunk.css"
+  },
+  {
+    "revision": "ebe7e4f33ee6812983d7",
+    "url": "/static/js/2.ff3736e6.chunk.js"
+  },
+  {
+    "revision": "bc188490c7b0ef37736cd59d0d9aec19",
+    "url": "/static/js/2.ff3736e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec780b8b31151f3cb3bb",
+    "url": "/static/js/main.ce24827f.chunk.js"
+  },
+  {
+    "revision": "6d4ba6ce1a9c7e06b00c",
+    "url": "/static/js/runtime-main.7b894c69.js"
+  }
+]);
